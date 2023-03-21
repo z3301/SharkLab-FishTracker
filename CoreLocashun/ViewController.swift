5f3eaa08fbe673c4cf4b0d9ccf71b204b5c9be0a
z3301 <scoopnisker@gmail.com>
2023-03-21 19:55:18 -0400

    cdv writer working

--- CoreLocashun/ViewController.swift
+++ CoreLocashun/ViewController.swift
@@ -22,7 +22,15 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     var timer:Timer = Timer()
     var count:Int = 0
     var timerCounting:Bool = false
-    var locationGlobal:String = ""
+    
+    
+    
+    var sFileName:String = ""
+    var documentDirectoryPath:String = ""
+    var documentURL:URL = URL(fileURLWithPath: "")
+    var output = OutputStream.toMemory()
+    var csvWriter = CHCSVWriter.init(forWritingToCSVFile: "")
+    var arrOfData = [[String]]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,16 +54,13 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             //            startStopButton.setTitleColor(UIColor.red, for: .normal)
             timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerCounter), userInfo: nil, repeats: true)
             
-            let sFileName = "test.csv"
-            
-            let documentDirectoryPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as String
-            
-            let documentURL = URL(fileURLWithPath: documentDirectoryPath).appendingPathComponent(sFileName)
-            
-            let output = OutputStream.toMemory()
-            
-            let csvWriter = CHCSVWriter(outputStream: output, encoding: String.Encoding.utf8.rawValue, delimiter: ",".utf16.first!)
-            
+            // csv vars
+            sFileName = "test.csv"
+            documentDirectoryPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as String
+            documentURL = URL(fileURLWithPath: documentDirectoryPath).appendingPathComponent(sFileName)
+            output = OutputStream.toMemory()
+            csvWriter = CHCSVWriter(outputStream: output, encoding: String.Encoding.utf8.rawValue, delimiter: ",".utf16.first!)
+                // csv header row
                 csvWriter?.writeField("DATE")
                 csvWriter?.writeField("TIME")
                 csvWriter?.writeField("LATITUDE")
@@ -73,44 +78,9 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                 csvWriter?.writeField("FISH SCHOOL")
                 csvWriter?.writeField("NOTES")
                 csvWriter?.finishLine()
-            
-                // array to add data
-                var arrOfData = [[String]]()
-                
-                arrOfData.append(["3/26/23","13:56:01","37.785834","-122.406417","","","","","","","1","","","","",""])
-                arrOfData.append(["3/26/23","13:56:02","37.785834","-122.406417","","","1","","","","","","","","",""])
-                arrOfData.append(["3/26/23","13:56:03","37.785834","-122.406417","","","","","","1","","","","","",""])
-            
-                for(elements) in arrOfData.enumerated() {
-                    csvWriter?.writeField(elements.element[0]) // DATE
-                    csvWriter?.writeField(elements.element[1]) // TIME
-                    csvWriter?.writeField(elements.element[2]) // LAT
-                    csvWriter?.writeField(elements.element[3]) // LONG
-                    csvWriter?.writeField(elements.element[4]) // FISHING BOAT
-                    csvWriter?.writeField(elements.element[5]) // SAILBOAT
-                    csvWriter?.writeField(elements.element[6]) // POWER BOAT
-                    csvWriter?.writeField(elements.element[7]) // TARPON
-                    csvWriter?.writeField(elements.element[8]) // SHARK
-                    csvWriter?.writeField(elements.element[9]) // NURSE SHARK
-                    csvWriter?.writeField(elements.element[10]) // EAGLE RAY
-                    csvWriter?.writeField(elements.element[11]) // STINGRAY
-                    csvWriter?.writeField(elements.element[12]) // DOLPHIN
-                    csvWriter?.writeField(elements.element[13]) // TURTLE
-                    csvWriter?.writeField(elements.element[14]) // FISH SCHOOL
-                    csvWriter?.writeField(elements.element[15]) // NOTES
-                    csvWriter?.finishLine()
-                }
-                csvWriter?.closeStream()
-                
-            let buffer = (output.property(forKey: .dataWrittenToMemoryStreamKey) as? Data)!
-            
-            do {
-                try buffer.write(to: documentURL)
-            }
-            catch {
-            }
         }
     }
+    
     @IBAction func resetTapped(_ sender: Any)
     {
         let alert = UIAlertController(title: "Stop Timer?", message: "Stopping the timer will save the file and reset the app. Are you sure you want to do that?", preferredStyle: .alert)
@@ -119,11 +89,41 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         }))
         
         alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { (_) in
+            
+            // build the CSV and close out the file
+            for(elements) in self.arrOfData.enumerated() {
+                self.csvWriter?.writeField(elements.element[0]) // DATE
+                self.csvWriter?.writeField(elements.element[1]) // TIME
+                self.csvWriter?.writeField(elements.element[2]) // LAT
+                self.csvWriter?.writeField(elements.element[3]) // LONG
+                self.csvWriter?.writeField(elements.element[4]) // FISHING BOAT
+                self.csvWriter?.writeField(elements.element[5]) // SAILBOAT
+                self.csvWriter?.writeField(elements.element[6]) // POWER BOAT
+                self.csvWriter?.writeField(elements.element[7]) // TARPON
+                self.csvWriter?.writeField(elements.element[8]) // SHARK
+                self.csvWriter?.writeField(elements.element[9]) // NURSE SHARK
+                self.csvWriter?.writeField(elements.element[10]) // EAGLE RAY
+                self.csvWriter?.writeField(elements.element[11]) // STINGRAY
+                self.csvWriter?.writeField(elements.element[12]) // DOLPHIN
+                self.csvWriter?.writeField(elements.element[13]) // TURTLE
+                self.csvWriter?.writeField(elements.element[14]) // FISH SCHOOL
+                self.csvWriter?.writeField(elements.element[15]) // NOTES
+                self.csvWriter?.finishLine()
+            }
+            self.csvWriter?.closeStream()
+            let buffer = (self.output.property(forKey: .dataWrittenToMemoryStreamKey) as? Data)!
+            do {
+                try buffer.write(to: self.documentURL)
+            }
+            catch {
+            }
+            
+            // reset the display
             self.count = 0
             self.timer.invalidate()
             self.timerDisplay.text = self.makeTimeString(hours: 0, minutes: 0, seconds: 0)
-//            self.startStopButton.setTitle("Start", for: .normal)
-//            self.startStopButton.setTitleColor(UIColor.white, for: .normal)
+            //            self.startStopButton.setTitle("Start", for: .normal)
+            //            self.startStopButton.setTitleColor(UIColor.white, for: .normal)
         }))
         
         self.present(alert, animated: true, completion: nil)
@@ -137,8 +137,18 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         let timeString = makeTimeString(hours: time.0, minutes: time.1, seconds: time.2)
         timerDisplay.text = timeString
         
+        // vars
+        let lat:String! = latitude.text
+        let long:String! = longitude.text
+
+        let date = Date()
+        let today = date.formatted(date: .numeric, time: .omitted)
+        let now = date.formatted(date: .omitted, time: .complete)
+
         // csv code
-        csv.
+        arrOfData.append([today,now,lat,long,"","","","","","","","","","","",""])
+        
+     
     }
     
     func secondsToHoursMinutesSeconds(seconds: Int) -> (Int, Int, Int)
