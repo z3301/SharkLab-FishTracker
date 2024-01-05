9638fb3f6576767ed71f0fbe4ff28ec190d24b09
z3301 <scoopnisker@gmail.com>
2024-01-05 17:35:07 -0500

    major changes

---
+++ CoreLocashun/ClarityViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ClarityViewController.swift
+//  CoreLocashun
+//
+//  Created by Daniel Zimmerman on 1/5/24.
+//
+
+import UIKit
+
+class ClarityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
